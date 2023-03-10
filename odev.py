bac8511a8c460fc696690bf1cca84e25fff9f2a1
Yaren Mamuk <77510962+yarenmamuk@users.noreply.github.com>
2023-03-10 21:23:32 +0300

    Add files via upload

---
+++ odev.py
@@ -0,0 +1,24 @@
+# int,byte : Tam sayı
+# float, double : Ondalıklı sayı
+# str: metinsel veya karakter ifadeleri
+# bool : Mantıksal ifadeler (Doğru ve Yanlış)
+
+# ödevin yüzde kaç tamamlandığı string değişkeniyle tanımlanıyor
+# siteye giriş kontrolü boolean
+
+# Kodlamaio sayfasına girerken mail ve şifre
+mail = True
+password = True
+if mail == True & password == True:
+    print("Giriş başarılı")
+else:
+    print("E-mail ya da şifre hatalı")
+
+#ödev tamamlama yüzdesi
+homework = "%100"
+if homework == "100":
+    print("tik işareti ve ödevi bitir devam et")
+else:
+    print("ödev tamamlanmadı")
+
+
